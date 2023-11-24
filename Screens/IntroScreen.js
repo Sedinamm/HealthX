d1c7486c4998e665ed25aed84f8a222a0b155e99
Sedinam <sedinam436@gmail.com>
2023-11-24 13:40:19 -0300

    imported IntroScreen in App.js

---
+++ Screens/IntroScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React from "react";
+
+const IntroScreen = () => {
+  return (
+    <View>
+      <Text>IntroScreen</Text>
+    </View>
+  );
+};
+
+export default IntroScreen;
+
+const styles = StyleSheet.create({});
