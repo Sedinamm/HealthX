c42356642aa3fcc1b8b9e6ac3cef99875b05e6e1
Sedinam <sedinam436@gmail.com>
2023-11-25 13:10:11 -0300

    Added a signup screen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import IntroScreen from "./Screens/IntroScreen";
+import SignupScreen from "./Screens/SignupScreen";
 
 export default function App() {
   return (
     <View>
       <IntroScreen />
+      <SignupScreen />
     </View>
   );
 }
