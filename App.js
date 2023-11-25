9124bcaf745e48ff72ccdf5b4a21e13ff61c8757
Sedinam <sedinam436@gmail.com>
2023-11-25 13:11:34 -0300

    imported the login screen to App.js

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import IntroScreen from "./Screens/IntroScreen";
 import SignupScreen from "./Screens/SignupScreen";
+import LoginScreen from "./Screens/LoginScreen";
 
 export default function App() {
   return (
     <View>
       <IntroScreen />
       <SignupScreen />
+      <LoginScreen />
     </View>
   );
 }
