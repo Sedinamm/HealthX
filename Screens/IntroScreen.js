07c6af15149e4c8a1285d601f08111bfb398c440
Sedinam <sedinam436@gmail.com>
2023-11-30 17:11:13 -0300

    added a video to the assets folder

--- Screens/IntroScreen.js
+++ Screens/IntroScreen.js
@@ -1,14 +1,31 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import Video from "react-native-video";
 
 const IntroScreen = () => {
   return (
     <View>
-      <Text>IntroScreen</Text>
+      <Video
+        source={require("../assets/1080p.mp4")}
+        style={styles.backgroundVideo}
+        volume={1.0}
+        muted={false}
+        resizeMode={"cover"}
+        rate={1.0}
+        ignoreSilentSwitch={"obey"}
+      />
     </View>
   );
 };
 
 export default IntroScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  backgroundVideo: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+});
