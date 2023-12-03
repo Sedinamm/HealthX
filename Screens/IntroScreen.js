341d89f6ca6d2b257df78aea118ccedd97db188b
Sedinam <sedinam436@gmail.com>
2023-12-03 06:46:32 -0300

    added an image and fixed styles

--- Screens/IntroScreen.js
+++ Screens/IntroScreen.js
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  SafeAreaView,
+} from "react-native";
 import React from "react";
 
 const IntroScreen = () => {
   return (
-    <View>
-      <Text>Hello world</Text>
+    <View style={styles.container}>
+      <View style={{ alignItems: "center" }}>
+        <Image
+          style={styles.introImage1}
+          source={require("../Images/hx1.png")}
+        />
+      </View>
+
+      {/* Description */}
+      <View>
+        <Text style={{ color: "white" }}>
+          Hello World This is a Description
+        </Text>
+      </View>
     </View>
   );
 };
 
 export default IntroScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+  introImage1: {
+    height: 450,
+    width: 500,
+    right: 50,
+    top: 50,
+  },
+});
