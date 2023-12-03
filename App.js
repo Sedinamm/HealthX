542c77f7a5c9b9c125138ac508418431bcb240a8
Sedinam <sedinam436@gmail.com>
2023-12-03 06:09:41 -0300

    Added navigation container and stack screens to App.js

--- App.js
+++ App.js
@@ -5,14 +5,20 @@ import SignupScreen from "./Screens/SignupScreen";
 import LoginScreen from "./Screens/LoginScreen";
 import HomeScreen from "./Screens/HomeScreen";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View>
-      <IntroScreen />
-      {/* <SignupScreen /> */}
-      {/* <LoginScreen /> */}
-      {/* <HomeScreen /> */}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="IntroScreen" component={IntroScreen} />
+        <Stack.Screen name="SignupScreen" component={SignupScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
