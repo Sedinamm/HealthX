eae888cb56d52ed376cfdaeab2270786ff0651fd
Sedinam <sedinam436@gmail.com>
2023-12-03 06:48:33 -0300

    initial commit

--- Screens/IntroScreen.js
+++ Screens/IntroScreen.js
@@ -1,31 +1,14 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import React from "react";
-import video from "react-native-video";
 
 const IntroScreen = () => {
   return (
     <View>
-      <Video
-        source={require("../assets/1080p.mp4")}
-        style={styles.backgroundVideo}
-        volume={1.0}
-        muted={false}
-        resizeMode={"cover"}
-        rate={1.0}
-        ignoreSilentSwitch={"obey"}
-      />
+      <Text>Hello world</Text>
     </View>
   );
 };
 
 export default IntroScreen;
 
-const styles = StyleSheet.create({
-  backgroundVideo: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-  },
-});
+const styles = StyleSheet.create({});
