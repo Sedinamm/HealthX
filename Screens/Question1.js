079ba2468d8e04767b9df5ae90a2e9a325cb458e
Sedinam <sedinam436@gmail.com>
2023-12-04 11:17:40 -0300

    initial commit

--- Screens/Question1.js
+++ Screens/Question1.js
@@ -11,7 +11,7 @@ const Question1 = () => {
         style={{
           flex: 3.5,
           width: "100%",
-          backgroundColor: "green",
+          backgroundColor: "red",
           borderTopRightRadius: 40,
           borderTopLeftRadius: 40,
         }}
@@ -37,7 +37,56 @@ const Question1 = () => {
         >
           Let's get to know each other a little
         </Text>
-        <View></View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginTop: 20,
+            alignSelf: "center",
+          }}
+        >
+          <TouchableOpacity>
+            {/* Male */}
+            <View
+              style={{
+                backgroundColor: "#000",
+                height: 140,
+                width: 110,
+                borderRadius: 20,
+                marginRight: 10,
+              }}
+            >
+              <Image
+                style={{ width: "130%", height: "120%" }}
+                source={require("../Images/male.png")}
+              />
+              <Text style={{ color: "white" }}>Male</Text>
+            </View>
+          </TouchableOpacity>
+          {/* Female */}
+          <TouchableOpacity>
+            <View
+              style={{
+                backgroundColor: "#232227",
+                height: 140,
+                width: 110,
+                borderRadius: 20,
+                marginRight: 10,
+              }}
+            ></View>
+          </TouchableOpacity>
+          {/* Others */}
+          <TouchableOpacity>
+            <View
+              style={{
+                backgroundColor: "#232227",
+                height: 140,
+                width: 110,
+                borderRadius: 20,
+              }}
+            ></View>
+          </TouchableOpacity>
+        </View>
       </View>
       <View style={{ flex: 0.5, backgroundColor: "black" }}>
         <Text
