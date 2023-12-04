8640a14397cada9db3cb2d5105d4e21245fce4c6
Sedinam <sedinam436@gmail.com>
2023-12-04 12:00:03 -0300

    created a question1 screen

---
+++ Screens/Question1.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Question1 = () => {
+  return (
+    <View>
+      <Text>Question1</Text>
+    </View>
+  );
+};
+
+export default Question1;
+
+const styles = StyleSheet.create({});
