2b28b45431f9433019dbc9d6a344fef87d7d590d
Sedinam <sedinam436@gmail.com>
2024-01-02 10:23:14 -0300

    created a back button and a question number count

--- Screens/Question2.js
+++ Screens/Question2.js
@@ -11,13 +11,55 @@ import { Ionicons } from "@expo/vector-icons";
 const Question2 = () => {
   return (
     <SafeAreaView style={styles.container}>
-      {/* Back button and Number count */}
-      <View>
+      <View
+        style={{
+          flexDirection: "row",
+          paddingLeft: 20,
+          paddingRight: 20,
+          marginTop: 40,
+          justifyContent: "space-between",
+        }}
+      >
         {/* back button */}
-        <View></View>
+        <TouchableOpacity
+          style={{
+            height: 50,
+            width: 50,
+            backgroundColor: "#232227",
+            borderRadius: 50,
+          }}
+        >
+          <Ionicons
+            name="chevron-back-outline"
+            color="white"
+            size={32}
+            style={styles.backBtn}
+          />
+        </TouchableOpacity>
         {/* Number count */}
-        <View></View>
+        <View
+          style={{
+            height: 50,
+            width: 50,
+            backgroundColor: "#232227",
+            borderRadius: 50,
+          }}
+        >
+          <Text
+            style={{
+              color: "white",
+              textAlign: "center",
+              marginTop: 15,
+              fontSize: 16,
+              fontWeight: "bold",
+            }}
+          >
+            2/3
+          </Text>
+        </View>
       </View>
+      {/* Question */}
+      <View></View>
     </SafeAreaView>
   );
 };
@@ -28,5 +70,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 10,
     backgroundColor: "#000",
+  },
+  backBtn: {
+    alignSelf: "center",
+    marginTop: 5,
   },
 });
